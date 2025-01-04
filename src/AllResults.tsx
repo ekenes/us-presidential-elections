d1483c3a29a5d00474a89a9bb41a9ec951295482
Kristian Ekenes <kekenes@esri.com>
2025-01-03 16:54:53 -0800

    Add content

---
+++ src/AllResults.tsx
@@ -0,0 +1,42 @@
+import "./App.css";
+
+import "@esri/calcite-components/dist/components/calcite-block";
+import "@esri/calcite-components/dist/components/calcite-tab";
+import "@esri/calcite-components/dist/components/calcite-tab-nav";
+import "@esri/calcite-components/dist/components/calcite-tabs";
+import "@esri/calcite-components/dist/components/calcite-tab-title";
+
+import {
+  CalciteBlock,
+  CalciteTab,
+  CalciteTabNav,
+  CalciteTabs,
+  CalciteTabTitle,
+} from "@esri/calcite-components-react";
+
+function AllResults() {
+  return (
+    <>
+      <CalciteBlock heading="Historical results" open collapsible>
+       <CalciteTabs>
+        <CalciteTabNav slot="title-group">
+          <CalciteTabTitle selected>
+            Electoral Votes
+          </CalciteTabTitle>
+          <CalciteTabTitle>
+            Popular Votes
+          </CalciteTabTitle>
+        </CalciteTabNav>
+        <CalciteTab selected>
+          content here
+        </CalciteTab>
+        <CalciteTab>
+          content here
+        </CalciteTab>
+       </CalciteTabs>
+      </CalciteBlock>
+    </>
+  );
+}
+
+export default AllResults;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+// CDN hosted assets
+setAssetPath("https://js.arcgis.com/calcite-components/2.13.2/assets");
+
 import "@arcgis/map-components/dist/components/arcgis-map";
 import "@arcgis/map-components/dist/components/arcgis-legend";
-import {
-  ArcgisLayerList,
-  ArcgisLegend,
-  ArcgisMap,
-} from "@arcgis/map-components-react";
+import { ArcgisLegend, ArcgisMap } from "@arcgis/map-components-react";
 
 import "@esri/calcite-components/dist/components/calcite-action";
 import "@esri/calcite-components/dist/components/calcite-action-bar";
@@ -14,10 +14,12 @@ import "@esri/calcite-components/dist/components/calcite-panel";
 import "@esri/calcite-components/dist/components/calcite-shell";
 import "@esri/calcite-components/dist/components/calcite-shell-panel";
 import "@esri/calcite-components/dist/components/calcite-slider";
+import "@esri/calcite-components/dist/components/calcite-block";
 
 import {
   CalciteAction,
   CalciteActionBar,
+  CalciteBlock,
   CalcitePanel,
   CalciteShell,
   CalciteShellPanel,
@@ -28,6 +30,8 @@ import esriConfig from "@arcgis/core/config";
 import Popup from "@arcgis/core/widgets/Popup";
 
 import { useRef } from "react";
+import Examples from "./Examples";
+import AllResults from "./AllResults";
 
 esriConfig.applicationName = "U.S. Presidential Election Results (2000-2024)";
 
@@ -122,7 +126,12 @@ function App() {
               text="Layers"
             />
           </CalciteActionBar>
-          <CalcitePanel heading="Layers" id="layers" data-panel-id="layers" scale="m">
+          <CalcitePanel
+            heading="Layers"
+            id="layers"
+            data-panel-id="layers"
+            scale="m"
+          >
             Select an election year
             <CalciteSlider
               min={2000}
@@ -135,6 +144,25 @@ function App() {
               step={4}
               snap
             ></CalciteSlider>
+            <CalciteBlock heading="How to read this map" collapsible open>
+              <p>
+                This map shows the results of each of the previous 5 U.S.
+                presidential elections from 2000 to 2024. Each square represents
+                the election winner for the given area in one year. The most
+                recent election (2024) is represented as the right-most square.
+                Each square's color represents the winner of the election; its
+                size is proportional to the margin of victory for the winner.
+                Smaller squares indicate a closer election. Larger squares
+                indicate a larger margin of victory.
+              </p>
+              <img
+                src="./trend-legend-horizontal.png"
+                className="legend"
+                alt="Legend"
+              />
+            </CalciteBlock>
+            <Examples />
+            <AllResults />
           </CalcitePanel>
         </CalciteShellPanel>
         <ArcgisMap
