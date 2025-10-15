889a2547c534e33bd6268a7191ccaac899886db1
Kristian Ekenes <kekenes@esri.com>
2025-10-15 16:42:32 -0400

    update deploy options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/us-presidential-elections/",
+  build: {
+    outDir: "dist",
+    assetsDir: "public",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
